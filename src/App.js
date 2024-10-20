eafc01d20f6e2ff65fc79555544ddb415982e115
Kawal Jain <kawaljain03@gmail.com>
2024-10-20 13:47:20 +0530

    adding analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Header from "./components/Header";
 import Markdown from "./pages/Markdown";
@@ -8,8 +8,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./assets/css/global.css";
 
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALTICS_MEASUREMENT_ID);
+
 function App() {
   const [handleEditorViewMobile, setHandleEditorViewMobile] = useState(false);
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
+
   return (
     <div className="main">
       <Header
